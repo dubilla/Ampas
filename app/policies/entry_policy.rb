66f188bdfb5803b2031a9d40e813d4b2f2cf1b06
Dan Ubilla <dan.ubilla@gmail.com>
2017-02-26 17:27:22 -0500

    Allow entries to be viewed after they are locked

--- app/policies/entry_policy.rb
+++ app/policies/entry_policy.rb
@@ -1,6 +1,6 @@
 class EntryPolicy < ApplicationPolicy
   def show?
-    user.present? && user.entries.include?(record) && !record.locked?
+    (user.present? && user.entries.include?(record)) || record.locked?
   end
 
   def edit?
